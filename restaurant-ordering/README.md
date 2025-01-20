fc8179eaaca2ec74ee3c8fac085a05394fcf47b1
blockchaincavs <blockchaincavs@gmail.com>
2025-01-20 08:25:21 -0800

    inital working restaurant-ordering app

---
+++ restaurant-ordering/README.md
@@ -0,0 +1,7 @@
+# [Restaurant Ordering App](https://restaurant-ordering-app-cavs.netlify.app/)
+
+## Requirments
+- Follow the design spec
+- Render the three menu options using JS
+- Be able to add/remove items
+- Have a payment modal with compulsory form inputs
---
+++ restaurant-ordering/index.js
@@ -0,0 +1,81 @@
+import { menuArray } from "./data.js";
+
+const orderSection = document.getElementById("order-section");
+let itemsSelected = [];
+let totalPrice = 0;
+
+/**
+ * Handle Event for all clicks. 
+ * Refer to e.target.dataset for which button was clicked
+ */
+document.addEventListener("click", function(e) {
+
+    const { buttonId } = e.target.dataset;
+
+    if (buttonId) {
+        addToOrder(Number(buttonId));
+    } else if (e.target.id === "btn-complete") {
+        console.log(e.target.id)
+    }
+    
+});
+
+function addToOrder(buttonId) {
+
+    if (buttonId < 0 || buttonId >= menuArray.length) {
+        return;
+    }
+    
+    if (orderSection.style.display === '') {
+        orderSection.style.display = 'flex';
+    }
+
+    const menuItem = menuArray.at(buttonId);
+
+    itemsSelected.push(`
+        <div class="order-item flex">
+                    <p>${menuItem.name}</p>
+                    <p class="item-remove">remove</p>
+                    <p class="total-item-price">${menuItem.price}</p>
+        </div>
+    `)
+
+    totalPrice += menuItem.price;
+
+    orderSection.innerHTML = `
+        <h3>Your order</h3> 
+        ${itemsSelected.join('')} 
+        <div class="total-div flex">
+                    <p>Total price:</p>
+                    <p>${totalPrice}</p>
+        </div>
+        <button id="btn-complete">Complete order</button>
+    `;
+
+}
+
+function render() {
+    document.getElementById('main-content').innerHTML = getContentHtml();
+}
+
+function getContentHtml() {
+    let contentHtml = ``;
+
+    menuArray.forEach( item => {
+        contentHtml += `
+            <section class="item-content">
+                <p class="food-emoji">${item.emoji}</p>
+                <div class="item-description">
+                    <p class="food-item">${item.name}</p>
+                    <p class="food-toppings">${item.ingredients}</p>
+                    <p class="food-price">${item.price}</p>
+                </div>
+                <button class="add-btn" data-button-Id=${item.id}>+</button>
+            </section>
+        `   
+    });
+
+    return contentHtml;
+}
+
+render();
